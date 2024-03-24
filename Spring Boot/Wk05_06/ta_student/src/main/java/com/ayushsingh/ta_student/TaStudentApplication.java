af96a62b6b5a9992c2846f0e1c0053ca533f83d4
singhayush20 <ayushsingh20november@gmail.com>
2024-03-24 13:38:23 +0530

    talent acquisition starter for student

---
+++ Spring Boot/Wk05_06/ta_student/src/main/java/com/ayushsingh/ta_student/TaStudentApplication.java
@@ -0,0 +1,13 @@
+package com.ayushsingh.ta_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaStudentApplication.class, args);
+    }
+
+}
---
+++ Spring Boot/Wk05_06/ta_student/src/test/java/com/ayushsingh/ta_student/TaStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushsingh.ta_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
